9863a48f81a99ba882c84d8e7024bf93aaf08b3e
marathicoders <90388948+21prafull@users.noreply.github.com>
2023-02-28 14:42:47 +0530

    setup gh-pages homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { Routes, Route} from "react-router-dom";
 import About from "./routes/About";
-import Careers from "./routes/Careers";
 import Home from "./routes/Home";
 import Navbar from './Navbar';
 function App() {
@@ -11,7 +10,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/careers" element={<Careers />} />
       </Routes>
     </>
   );
